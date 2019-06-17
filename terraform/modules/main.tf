968ed6cd95417fc8ff79b394b88127614db00496
nonzeronucleus <gitkraken@ianplumb.org>
2019-06-17 23:05:20 +0100

    Commented out cdn

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -51,52 +51,52 @@ resource "aws_route53_record" "root_domain" {
   }
 }
 
-resource "aws_cloudfront_distribution" "cdn" {
-  origin {
-    origin_id   = "${var.subdomain}.${var.domain}"
-    domain_name = "${var.subdomain}.${var.domain}.s3.amazonaws.com"
-  }
+# resource "aws_cloudfront_distribution" "cdn" {
+#   origin {
+#     origin_id   = "${var.subdomain}.${var.domain}"
+#     domain_name = "${var.subdomain}.${var.domain}.s3.amazonaws.com"
+#   }
 
-  # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
-  aliases = ["${var.subdomain}.${var.domain}"]
+#   # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
+#   aliases = ["${var.subdomain}.${var.domain}"]
 
-  enabled             = true
-  default_root_object = "index.html"
+#   enabled             = true
+#   default_root_object = "index.html"
 
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.subdomain}.${var.domain}"
+#   default_cache_behavior {
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "${var.subdomain}.${var.domain}"
 
-    forwarded_values {
-      query_string = true
-      cookies {
-        forward = "none"
-      }
-    }
+#     forwarded_values {
+#       query_string = true
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-  # The cheapest priceclass
-  price_class = "PriceClass_100"
+#   # The cheapest priceclass
+#   price_class = "PriceClass_100"
 
-  # This is required to be specified even if it's not used.
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      locations        = []
-    }
-  }
+#   # This is required to be specified even if it's not used.
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#       locations        = []
+#     }
+#   }
 
-  viewer_certificate {
-      acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
-      ssl_support_method ="sni-only"
-  }
-}
+#   viewer_certificate {
+#       acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
+#       ssl_support_method ="sni-only"
+#   }
+# }
 
 
 
