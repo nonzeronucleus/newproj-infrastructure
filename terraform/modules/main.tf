220720e166b9c10d5812959d017715d1233295ff
nonzeronucleus <gitkraken@ianplumb.org>
2019-06-17 23:10:07 +0100

    Removes r53

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -33,23 +33,23 @@ EOF
   }
 }
 
-data "aws_route53_zone" "main" {
-  name         = "${var.domain}"
-  private_zone = false
-}
+# data "aws_route53_zone" "main" {
+#   name         = "${var.domain}"
+#   private_zone = false
+# }
 
 
-resource "aws_route53_record" "root_domain" {
-  zone_id = "${data.aws_route53_zone.main.zone_id}"
-  name = "${var.subdomain}.${var.domain}"
-  type = "A"
+# resource "aws_route53_record" "root_domain" {
+#   zone_id = "${data.aws_route53_zone.main.zone_id}"
+#   name = "${var.subdomain}.${var.domain}"
+#   type = "A"
 
-  # alias {
-  #   name = "${aws_cloudfront_distribution.cdn.domain_name}"
-  #   zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
-  #   evaluate_target_health = false
-  # }
-}
+#   alias {
+#     name = "${aws_cloudfront_distribution.cdn.domain_name}"
+#     zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+#     evaluate_target_health = false
+#   }
+# }
 
 # resource "aws_cloudfront_distribution" "cdn" {
 #   origin {
