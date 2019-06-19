d9954a038ffd8d56f614415d31984d13f6454aa7
nonzeronucleus <gitkraken@ianplumb.org>
2019-06-19 21:06:34 +0100

    Full tagging

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "site" {
   bucket = "${var.subdomain}.${var.domain}"
   acl = "public-read"
   tags = {
-    Name = "${var.subdomain}.${var.domain}"
+    site = "${var.subdomain}.${var.domain}"
   }
 
 
@@ -37,70 +37,80 @@ EOF
   }
 }
 
-# data "aws_route53_zone" "main" {
-#   name         = "${var.domain}"
-#   private_zone = false
-# }
+data "aws_route53_zone" "main" {
+  name         = "${var.domain}"
+  private_zone = false
+}
 
 
-# resource "aws_route53_record" "root_domain" {
-#   zone_id = "${data.aws_route53_zone.main.zone_id}"
-#   name = "${var.subdomain}.${var.domain}"
-#   type = "A"
+resource "aws_route53_record" "root_domain" {
+  zone_id = "${data.aws_route53_zone.main.zone_id}"
+  name = "${var.subdomain}.${var.domain}"
+  type = "A"
 
-#   alias {
-#     name = "${aws_cloudfront_distribution.cdn.domain_name}"
-#     zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
-#     evaluate_target_health = false
-#   }
-# }
+  tags = {
+    site = "${var.subdomain}.${var.domain}"
+  }
 
-# resource "aws_cloudfront_distribution" "cdn" {
-#   origin {
-#     origin_id   = "${var.subdomain}.${var.domain}"
-#     domain_name = "${var.subdomain}.${var.domain}.s3.amazonaws.com"
-#   }
 
-#   # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
-#   aliases = ["${var.subdomain}.${var.domain}"]
+  alias {
+    name = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
 
-#   enabled             = true
-#   default_root_object = "index.html"
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    origin_id   = "${var.subdomain}.${var.domain}"
+    domain_name = "${var.subdomain}.${var.domain}.s3.amazonaws.com"
+  }
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = "${var.subdomain}.${var.domain}"
+  tags = {
+    site = "${var.subdomain}.${var.domain}"
+  }
 
-#     forwarded_values {
-#       query_string = true
-#       cookies {
-#         forward = "none"
-#       }
-#     }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+  # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
+  aliases = ["${var.subdomain}.${var.domain}"]
 
-#   # The cheapest priceclass
-#   price_class = "PriceClass_100"
+  enabled             = true
+  default_root_object = "index.html"
 
-#   # This is required to be specified even if it's not used.
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#       locations        = []
-#     }
-#   }
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.subdomain}.${var.domain}"
 
-#   viewer_certificate {
-#       acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
-#       ssl_support_method ="sni-only"
-#   }
-# }
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # The cheapest priceclass
+  price_class = "PriceClass_100"
+
+  # This is required to be specified even if it's not used.
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+      acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
+      ssl_support_method ="sni-only"
+  }
+}
 
 
 
